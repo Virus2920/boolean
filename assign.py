3400d499272762cb63811fff63fe87c57a558fa3
Virus2920 <111279768+Virus2920@users.noreply.github.com>
2023-04-09 14:57:58 -0400

    Update assign.py

--- assign.py
+++ assign.py
@@ -1,11 +1,7 @@
-def has_common_member(list1, list2, list3, list4, list5, list6, list7, list8, list9, list10):
+def check_common_member(lst11, lst12, lst13, lst14, lst15, lst16, lst17, lst18, lst19, lst20):
     """
-    This function takes 10 lists as input and returns True if they have at least one common member.
+    Returns True if any of the 10 input lists have at least one common element, False otherwise
     """
-    for element in list1:
-        if element in list2 or element in list3 or element in list4 or element in list5 or element in list6 or element in list7 or element in list8 or element in list9 or element in list10:
-            return True
-    return False
 
 list1 = [1, 2, 3]
 list2 = [4, 5, 6]
@@ -18,13 +14,10 @@ list8 = [22, 23, 24]
 list9 = [25, 26, 27]
 list10 = [28, 29, 30]
 
-if has_common_member(list1, list2, list3, list4, list5, list6, list7, list8, list9, list10):
-    print("TRUE")
-else:
-    print("FALSE")
+print( bool (check_common_member(lst11, lst12, lst13, lst14, lst15, lst16, lst17, lst18, lst19, lst20)))
 
 
-has_common_member(list1, list2, list3, list4, list5, list6, list7, list8, list9, list10)
+check_common_member(lst11, lst12, lst13, lst14, lst15, lst16, lst17, lst18, lst19, lst20)
 
 
 
