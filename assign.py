cc94257a30d57527afadc78b1ab36d033723fd99
Virus2920 <111279768+Virus2920@users.noreply.github.com>
2023-04-11 10:03:22 -0400

    Update assign.py

--- assign.py
+++ assign.py
@@ -1,12 +1,8 @@
-def check_common_member(lst11, lst12, lst13, lst14, lst15, lst16, lst17, lst18, lst19, lst20):
-    """
-    Returns True if any of the 10 input lists have at least one common element, False otherwise
-    """
-
+# List of numbers
 list1 = [1, 2, 3]
 list2 = [4, 5, 6]
 list3 = [7, 8, 9]
-list4 = [10, 11, 12]
+list4 = [10, 1, 12]
 list5 = [13, 14, 15]
 list6 = [16, 17, 18]
 list7 = [19, 20, 21]
@@ -14,10 +10,24 @@ list8 = [22, 23, 24]
 list9 = [25, 26, 27]
 list10 = [28, 29, 30]
 
-print( bool (check_common_member(lst11, lst12, lst13, lst14, lst15, lst16, lst17, lst18, lst19, lst20)))
+# getting the common elements by using set intersection 
+common = set(list1) & set(list2) or set(list1) & set(list3) or set(list1) & set(list4) or set(list1)\
+        & set(list5) or set(list1) & set(list6) or set(list1) & set(list7) or set(list1) & set(list8)\
+        or set(list1) & set(list9) or set(list1) & set(list10) or set(list2) & set(list3) or set(list2)\
+        & set(list4) or set(list2) & set(list5) or set(list2)& set(list6) or set(list2) & set(list7)\
+        or set(list2) & set(list8) or set(list2)& set(list9) or set(list2) & set(list10)\
+        or set(list3) & set(list4) or set(list3)& set(list5) or set(list3) & set(list6) or set(list3)& set(list7)\
+        or set(list3) & set(list8) or set(list3)& set(list9) or set(list3) & set(list10) or set(list4)& set(list5)\
+        or set(list4) & set(list6) or set(list4)& set(list7) or set(list4) & set(list8) or set(list4)& set(list9)\
+        or set(list4) & set(list10) or set(list5)& set(list6) or set(list5) & set(list7) or set(list5)& set(list8)\
+        or set(list5) & set(list9) or set(list5)& set(list10) or set(list6) & set(list7) or set(list6)& set(list8)\
+        or set(list6) & set(list9) or set(list6)& set(list10) or set(list7) & set(list8) or set(list7)& set(list9)\
+        or set(list7) & set(list10) or set(list8)& set(list9) or set(list8) & set(list10) or set(list9)& set(list10)
 
-
-check_common_member(lst11, lst12, lst13, lst14, lst15, lst16, lst17, lst18, lst19, lst20)
+if common:
+    print(bool(common))
+else:
+    print(bool())
 
 
 
