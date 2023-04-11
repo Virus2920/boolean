6e97255cae81de64acd5a2ea4cefbf4df2f0b0f9
Virus2920 <111279768+Virus2920@users.noreply.github.com>
2023-04-11 10:22:13 -0400

    Update age_calculator.py

--- age_calculator.py
+++ age_calculator.py
@@ -1,13 +1,17 @@
 def is_student_adult():
-    name = input("input your name: ")
+    name = input("What is your name: ")
     date_of_birth = int(input("date of birth: "))
     current_year = 2023  
     age = current_year - date_of_birth
     if age >= 18:
-       print (bool(age,"years old.")) 
+        print(bool(age)) 
+        print(name + ", you are " + str(age) + " Years old: qualified")
         
     else:
-        print(bool(18 < age))
+        print(bool())
+        print("Adult only! You are " + str(age) + " Years old: Not qualified.")
+        exit()
+        
         
 
 
